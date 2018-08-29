c9632415aae84ae51bb733b4c497cc697c57109f
hushicai <bluthcy@gmail.com>
2018-08-29 23:11:41 +0800

    add web entry

---
+++ index.web.js
@@ -0,0 +1,5 @@
+
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+AppRegistry.registerComponent('reactuniversalapp', () => App);
+AppRegistry.runApplication('App', {rootTag: document.getElementById('app') });
